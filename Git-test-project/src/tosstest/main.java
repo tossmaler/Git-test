4e2c551db671160b19f3c4a13592a91ec1fd1be4
апапо <@VAIO>
2015-04-20 14:43:58 +0300

    add

--- Git-test-project/src/tosstest/main.java
+++ Git-test-project/src/tosstest/main.java
@@ -3,7 +3,8 @@ package tosstest;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("heloo for");
+		System.out.println("heloo forfdsfd");
+		System.out.println("heloo forfdsfd");
 
 	}
 
